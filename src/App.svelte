937cb5e45020ad4ad8cf6dc0d642579647727248
Wim Horinga <wim.horinga@exlibrisgroup.com>
2021-01-04 10:16:41 +0100

    Happy new year!

--- src/App.svelte
+++ src/App.svelte
@@ -37,27 +37,11 @@
 <main>
   <h1>{name}</h1>
   <p>
-    Iedereen heeft al niet zo'n best jaar, en dan ook nog eens geen vuurwerk...
+    Gelukkig nieuw jaar!!!!
   </p>
 
-  <h2>Geen vuurwerkshow maar een lichtshow!</h2>
-  <div class="main-text">
-    <p>
-      Een oproep aan alle theaters, discotheken, DJ's, evenement
-      organisaties en enthousiastelingen!
-    </p>
-    <p>
-      Zet op 31 december je lampen, lasers en rookmachines voor je raam, of op
-      je dak, en organiseer een buitenlichtshow!
-    </p>
-    <p>
-      Dan hebben we toch nog iets moois om naar te kijken, en kunnen we 2020 op
-      een leuke manier de rug toekeren.
-    </p>
-
-  </div>
-
+ 
   <img src="laserop2020.png" alt="Laser op 2020 afbeelding" />
-  <footer>Pas natuurlijk wel op dat je niemand in de weg zit met je lichten ;-) - En zorg ervoor dat er geen menigten ontstaan; het moet wel veilig zijn!</footer>
+  <footer>Lang leve lasers!</footer>
 
 </main>
